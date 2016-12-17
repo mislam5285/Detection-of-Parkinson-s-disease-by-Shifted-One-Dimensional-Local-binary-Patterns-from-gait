cf0259b64ea3b7b68f87e66eab883b287feb8c21
riokt <rizqi.okta@gmail.com>
2016-12-17 23:31:54 +0700

    ngedi shifted dan nambah folder

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # eFPeKomBio
 Detection of Parkinson's disease by Shifted One Dimensional Local binary Patterns from gait
+
+1 s.d 10 Data Train
+11 s.d 17 Data Test
--- shifted1DLBP.m
+++ shifted1DLBP.m
@@ -1,8 +1,8 @@
-function lbphist=shifted1DLBP(signal,PL)
+function [lbphist,newSignal]=shifted1DLBP(signal,PL)
     sz=size(signal,2);
     PR=8-PL;
     PC=PL+1;
-    %newSignal=zeros(1:sz);
+    newSignal=zeros(1,sz);
     signal=[zeros(1,PL) signal zeros(1,PR)];
     for i=PL+1:PL+sz
         conv=signal(i-PL:i+PR);
@@ -12,5 +12,5 @@ function lbphist=shifted1DLBP(signal,PL)
         newData=[conv(1:PL) conv(PC+1:PC+PR)];
         newSignal(i-PL)=bi2de(newData,'left-msb');
     end
-    lbphist = imhist(newSignal);
+    lbphist = hist(newSignal,0:255);
 end
